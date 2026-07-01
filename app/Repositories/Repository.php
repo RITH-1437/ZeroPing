4abfd4d6dbf1a74b2b2d455f819b410e88cc3a79
RITH-1437 <neyrithneyrith@gmail.com>
2026-07-01 23:15:04 +0700

    [FEATURE]: add base repository.

---
+++ app/Repositories/Repository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Core\Database\Model;
+
+abstract class Repository
+{
+    protected Model $model;
+
+    public function all(): array
+    {
+        return $this->model->all();
+    }
+
+    public function find(int $id): ?array
+    {
+        return $this->model->find($id);
+    }
+
+    public function create(array $data): bool
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(int $id, array $data): bool
+    {
+        return $this->model->update($id, $data);
+    }
+
+    public function delete(int $id): bool
+    {
+        return $this->model->delete($id);
+    }
+}
