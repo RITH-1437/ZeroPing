8c756324b5b9379f43c7737ec40a9bc78e164699
RITH-1437 <neyrithneyrith@gmail.com>
2026-07-09 22:02:57 +0700

    fix: load helpers before App bootstrap in web entry point
    public/index.php now requires app/Helpers/helpers.php before bootstrap
    to ensure functions like storage_path() are available when config
    files are loaded during provider registration.

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ define('ZERO_PING_START', microtime(true));
 define('BASE_PATH', realpath(__DIR__ . '/..'));
 
 require_once BASE_PATH . '/vendor/autoload.php';
+require_once BASE_PATH . '/app/Helpers/helpers.php';
 
 $app = require_once BASE_PATH . '/bootstrap/app.php';
 
